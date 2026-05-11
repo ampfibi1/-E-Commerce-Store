3094ce3473fd2078b88952bcb47da1d132517092
Abdullah Al-Tamjid <168060724+ampfibi1@users.noreply.github.com>
2026-05-11 13:34:53 +0600

    dashboard

--- views/admin/analytics.php
+++ views/admin/analytics.php
@@ -1,2 +1,19 @@
 <?php
-    echo "analytics";
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Analytics</title>
+    <link rel="stylesheet" href="CSS/allmainContent.css">
+</head>
+<body>
+    <?php include 'sidebar.php'; ?>
+    <div class="main_content" >
+        <h1>Analytics Page</h1>
+        <p>This page will display various analytics and reports related to the e-commerce store.</p>
+    </div>
+</body>
+</html>
