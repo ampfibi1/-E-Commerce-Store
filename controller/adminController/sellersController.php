a40b0fb2bf1822d1f8fef2e191921bd7ce01e76e
Abdullah Al-Tamjid <168060724+ampfibi1@users.noreply.github.com>
2026-05-11 16:07:41 +0600

    add admin seller panel

---
+++ controller/adminController/sellersController.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+
+require_once '../../model/adminModel/sellersModel.php';
+require_once '../../model/connection.php';
+
+$conn = conn_open();
+
+if (isset($_GET['action']) && isset($_GET['id'])) {
+    $action = $_GET['action'];
+    $sellerId = $_GET['id'];
+    switch ($action) {
+        case 'approve':
+            approveSeller($sellerId, $conn);
+            break;
+
+        case 'reject':
+            rejectSeller($sellerId, $conn);
+            break;
+
+        case 'suspend':
+            suspendSeller($sellerId, $conn);
+            break;
+
+        case 'reactivate':
+            reactivateSeller($sellerId, $conn);
+            break;
+    }
+
+    echo "success";
+}
+
+$sellers = getAllSellers($conn);
+conn_close($conn);
+
+$_SESSION['sellers'] = $sellers;
+
+header("Location: ../../views/admin/sellers.php");
+exit();
+?>
